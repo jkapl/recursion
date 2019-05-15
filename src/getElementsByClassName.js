21b5e7f6f34b3e7192c7c1638e63ba27d1c6a346
Joel <joelkaplan1@gmail.com>
2019-05-15 12:52:01 -0700

    working on stringify and ClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var elemArr = [];
+
+    var getChildren = function(currentNode, elemArr) {
+      
+      for (var i=0; i<currentNode.childNodes.length; i++) {
+       if (currentNode.childNodes[i].classList && _.contains(currentNode.childNodes[i].classList, className)) {
+            elemArr.push(currentNode.childNodes[i]);
+          }
+        }
+        if (currentNode[i].childNodes[0]) {
+            getChildren(currentNode[i], elemArr);
+        }
+    }
+    getChildren(document, elemArr);
+    return elemArr;
 };
+
+
+//base case: if document.childNodes[i].hasChildNodes() === false return 
+
+//recursive case getElementsByClassName(childNodes)
+//if _.contains(document.childNodes[i].classList, className) { elemArr.push(document.childNodes[i]) }
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,69 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  //if current section is not an object or array
+  function elementLogic(elem) {
+    if (typeof elem === 'boolean' || typeof elem === 'number') {
+      return elem.toString();
+    }
+    if (elem === null) {
+      return 'null'
+    }
+    if (typeof elem === 'string') {
+      let result = '"'
+      result = result + elem + '"';
+      return result;
+    }
+  }
+  
+  //if current section is an obj invoke objIterate  
+  function objIterate(obj) {
+    var result = '{'
+    // for (var key in obj) {
+    //     result = result + '"' + key.toString() + '"' + ':' + obj[key] + ',';
+    // }    
+    for (var key in obj) {
+        result = result + '"' + elementLogic(key) + '"' + ':' + elementLogic(obj[key]) + ',';
+    }
+    return result.slice(0,result.length-1) + '}';
+  }
+  
+
+  //if current section is an array invoke arrIterate
+  function arrIterate(arr) {
+    if (arr.length === 0) { return '[]' }
+    var result = '['
+    for (var i=0; i<arr.length; i++) {
+      if (Array.isArray(arr[i])) { 
+        result = result + arrIterate(arr[i])
+      }
+      result = result + elementLogic(arr[i]) + ',';
+    }
+    return result.slice(0,result.length-1) + ']';
+  }
+
+  if (Array.isArray(obj)) {
+    return arrIterate(obj);
+  }
+
+  if (obj === null) {
+    return elementLogic(obj);
+  }
+
+  if (typeof obj === 'object') {
+   return objIterate(obj);
+  }
+  
+  return elementLogic(obj)
+
 };
+
+//[8, [[], 3, 4]]
+
+//JSON.stringify({ x: 5, y: 6 });
+// '{"x":5,"y":6}'
+
+//NaN, null, undefined, function() {} => null
+
+//{} => '{}'
